e4c69b5384654b8b20d7416662fdda35bbab1e86
AbhishekC81 <Abhishekchamoli73@gmail.com>
2024-01-02 04:36:26 +0530

    Added DetailsScreen functionality

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -2,16 +2,27 @@
 
 package com.example.movieapp.screens.details
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -26,18 +37,35 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.movieapp.screens.home.MainContent
+import coil.compose.rememberImagePainter
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
+import com.example.movieapp.widgets.MovieRow
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+
+
 @ExperimentalMaterial3Api
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun DetailsScreen(navController: NavController, 
-                  movieData: String?
+fun DetailsScreen(navController: NavController,
+                  movieId: String?
 ) {
+    
+    val newMovieList = getMovies().filter {movie -> 
+        movie.id == movieId
+    }
+
+    val mov= newMovieList.first()
 
     Scaffold(
         topBar = {
@@ -67,21 +95,209 @@ fun DetailsScreen(navController: NavController,
             modifier = Modifier
                 .fillMaxHeight()
                 .fillMaxWidth()
+                .padding(top = it.calculateTopPadding())
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Center,
+//                modifier = Modifier
+//                    .fillMaxSize()
+//                    .verticalScroll(rememberScrollState())
             ) {
-                Text(
-                    text = movieData.toString(),
-                    style = MaterialTheme.typography.headlineMedium
-                )
-                Spacer(modifier = Modifier.height(23.dp))
-                Button(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Text(text = "Go Back")
+
+//                MovieRow(movie = mov)
+//
+//                Spacer(modifier = Modifier.height(8.dp))
+//                Divider()
+//                Text(text = "Movie Images")
+//
+//                HorizontalImageView(mov)
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(150.dp),
+                    colors = CardDefaults.cardColors(Color.Black),
+                    border = BorderStroke(1.dp, Color.DarkGray),
+                    shape = RectangleShape
+                ) {
+                    Image(painter = rememberImagePainter(data = mov.images[0]),
+                        contentDescription = "Movie Poster",
+                        modifier = Modifier.fillMaxWidth())
                 }
+                Divider(modifier = Modifier.padding(3.dp))
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(10.dp),
+                    shape = RectangleShape
+                ) {
+                    Column(modifier = Modifier.padding(3.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = mov.title,
+                            fontWeight = FontWeight.Bold,
+                            style = MaterialTheme.typography.headlineLarge,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(2.dp)
+                        )
+                    }
+                }
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(10.dp),
+                    border = BorderStroke(1.dp, Color.DarkGray),
+                ) {
+                    Column(modifier = Modifier.padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                        Text(
+                            text = "Plot: ",
+                            fontWeight = FontWeight.SemiBold,
+                            style = MaterialTheme.typography.headlineSmall,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.padding(2.dp)
+                        )
+                        Text(
+                            text = mov.plot,
+                            fontWeight = FontWeight.Normal,
+                            style = MaterialTheme.typography.labelLarge
+                        )
+                    }
+                }
+
+                Divider(modifier = Modifier.padding(3.dp))
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    elevation = CardDefaults.cardElevation(10.dp),
+                    border = BorderStroke(1.dp, Color.DarkGray)
+                ) {
+                    Column(modifier = Modifier.padding(5.dp)) {
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    color = Color.Black
+                                )
+                            ) {
+                                append("Director: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.Normal,
+                                )
+                            ) {
+                                append(mov.director)
+                            }
+                        }, modifier = Modifier.padding(2.dp)
+                        )
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    color = Color.Black
+                                )
+                            ) {
+                                append("Released: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.Normal
+                                )
+                            ) {
+                                append(mov.year)
+                            }
+                        }, modifier = Modifier.padding(2.dp)
+                        )
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    color = Color.Black
+                                )
+                            ) {
+                                append("Genre: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.Normal
+                                )
+                            ) {
+                                append(mov.genre)
+                            }
+                        }, modifier = Modifier.padding(2.dp)
+                        )
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    color = Color.Black
+                                )
+                            ) {
+                                append("Cast: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.Normal
+                                )
+                            ) {
+                                append(mov.actors)
+                            }
+                        }, modifier = Modifier.padding(2.dp)
+                        )
+                        Text(buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.ExtraBold,
+                                    color = Color.Black
+                                )
+                            ) {
+                                append("Rating: ")
+                            }
+                            withStyle(
+                                style = SpanStyle(
+                                    fontSize = 6.sp,
+                                    fontWeight = FontWeight.Normal
+                                )
+                            ) {
+                                append(mov.rating)
+                            }
+                        }, modifier = Modifier.padding(2.dp)
+                        )
+                    }
+                }
+
+                Divider(modifier = Modifier.padding(3.dp))
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(5.dp)
+                        .height(200.dp),
+                    elevation = CardDefaults.cardElevation(10.dp),
+                    border = BorderStroke(1.dp, Color.DarkGray),
+                    shape = RectangleShape
+                ) {
+                    Text(text = "Movie Images",
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.headlineSmall,
+                        color = Color.Black
+                    )
+
+                    HorizontalImageView(newMovieList = newMovieList)
+                }
+
             }
         }
 
@@ -89,4 +305,24 @@ fun DetailsScreen(navController: NavController,
     }
 
 
+}
+
+@Composable
+private fun HorizontalImageView(newMovieList: List<Movie>) {
+    LazyRow() {
+        items(newMovieList[0].images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(4.dp)
+                    .size(200.dp),
+            ) {
+                Image(
+                    painter = rememberImagePainter(data = image),
+                    contentDescription = "Movie Image"
+                )
+
+            }
+
+        }
+    }
 }
